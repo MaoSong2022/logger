c0bb5001f64470d8a662979b5f55dda5cb86767c
MaoSong2022 <maosong@pjlab.org.cn>
2023-03-06 17:31:38 +0800

    ✨ feat(logger): add the level option

--- logger/logger.py
+++ logger/logger.py
@@ -1,11 +1,32 @@
 import logging
 
-APP_LOGGER_NAME = 'APP'
+APP_LOGGER_NAME: str = 'APP'
 
 
-def setup_app_level_logger(logger_name=APP_LOGGER_NAME, file_name="app_debug.log"):
+def setup_app_level_logger(logger_name: str=APP_LOGGER_NAME, 
+                           level: str='DEBUG',
+                           file_name: str="app_debug.log"):
+    """create a logger
+
+    Args:
+        logger_name (str, optional): name of the logger. Defaults to APP_LOGGER_NAME.
+        level (str, optional): controls the output level. Defaults to 'DEBUG'.
+        file_name (str, optional): path where the log is saved. Defaults to "app_debug.log".
+
+        level option: {
+            'CRITICAL': CRITICAL,
+            'FATAL': FATAL,
+            'ERROR': ERROR,
+            'WARN': WARNING,
+            'WARNING': WARNING,
+            'INFO': INFO,
+            'DEBUG': DEBUG,
+            'NOTSET': NOTSET,}
+    Returns:
+        _type_: the logger object
+    """
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(level)
     formatter = logging.Formatter(
         "[%(levelname)-s]:%(filename)s %(funcName)s [Line %(lineno)s] - %(message)s")
 
